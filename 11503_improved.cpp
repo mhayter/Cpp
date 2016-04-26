cf5bc71d98505386d750a4c98c286589aed28cd2
Michael Hayter <mhayter@umich.edu>
2016-04-25 17:48:16 -0700

    faster 11503 less waste map

--- 11503_improved.cpp
+++ 11503_improved.cpp
@@ -4,8 +4,137 @@
 #include <unordered_map>
 using namespace std;
 
+#include <cstdio>
+#include <cstring>
+
+//#2 eliminate map.count() == 0 0.060s
+//#3 with FastOoutput 0.090s -> 0.080s
 //#3 4/25/2016 0.011s -> 0.090s reserve
 //#5 8/12/2015 0.143s
+
+class FastOutput {
+	public:
+	FastOutput() {
+	m_dataOffset = 0;
+	}
+	~FastOutput() {
+		Flush();
+	}
+	void Flush() {
+	if (m_dataOffset) {
+	if (fwrite(m_data,1, m_dataOffset,stdout));
+	m_dataOffset = 0;
+	}
+	}
+
+	void PrintUint(uint32_t v, char d) {
+	if (m_dataOffset + 11 > sizeof(m_data)) Flush();
+	if (v < 100000) {
+	if (v < 1000) {
+	if (v < 10) {
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 1;
+	} else if (v < 100) {
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 2;
+	} else {
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 3;
+	}
+	} else {
+	if (v < 10000) {
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 4;
+	} else {
+	m_data[m_dataOffset + 4] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 5;
+	}
+	}
+	} else {
+	if (v < 100000000) {
+	if (v < 1000000) {
+	m_data[m_dataOffset + 5] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 4] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 6;
+	} else if (v < 10000000) {
+	m_data[m_dataOffset + 6] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 5] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 4] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 7;
+	} else {
+	m_data[m_dataOffset + 7] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 6] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 5] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 4] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 8;
+	}
+	} else {
+	if (v < 1000000000) {
+	m_data[m_dataOffset + 8] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 7] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 6] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 5] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 4] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 9;
+	} else {
+	m_data[m_dataOffset + 9] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 8] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 7] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 6] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 5] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 4] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 3] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 2] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 1] = v - v / 10 * 10 + 48; v /= 10;
+	m_data[m_dataOffset + 0] = v + 48;
+	m_dataOffset += 10;
+	}
+	}
+	}
+	m_data[m_dataOffset++] = d;
+	}
+	void PrintChar(char d) {
+	if (m_dataOffset + 1 > sizeof(m_data)) Flush();
+	m_data[m_dataOffset++] = d;
+	}
+	void ReplaceChar(int offset, char d) {
+	m_data[m_dataOffset + offset] = d;
+	}
+
+	void PrintStr(const char *str) {
+		if (strlen(str) + m_dataOffset > sizeof(m_data)) Flush();
+		while (m_data[m_dataOffset++] = *str++);
+	}
+	public:
+	uint8_t m_data[32768];
+	size_t m_dataOffset;
+}; 
 
 struct Subset{
 	int parent;
@@ -41,8 +170,9 @@ void Union(int x, int y) {
 int high=0;
 unordered_map<string,int>myMap;
 int getIndex(const string &s) {
-	if (myMap.count(s))
-		return myMap[s];
+	auto myIterator = myMap.find(s);
+	if (myIterator != myMap.end())
+		return myIterator->second;
 	else {
 		return myMap[s]=high++;
 	}
@@ -52,6 +182,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	myMap.reserve(MAX_FRIENDS);
+	FastOutput output;
 
 	int nCases; cin >> nCases;
 	for(int caseNum=1; caseNum<=nCases;caseNum++) {
@@ -76,7 +207,8 @@ int main() {
 			int v = adj[i].second;
 			
 			Union(u,v);
-			cout << subsets[find(u)].total << '\n';
+			output.PrintUint(subsets[find(u)].total,'\n');
+			//cout << subsets[find(u)].total << '\n';
 		}
 		//cout << endl;
 	}
